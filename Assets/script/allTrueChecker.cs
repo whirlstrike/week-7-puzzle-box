a965554f40f7164f6eaabad71e43fd9dbdb2698b
unknown <c.c.j.w.cw@gmail.com>
2025-03-17 02:43:56 +0000

    small additions
    started work o the next two levels, some minor bug fixing with the object checkers as well as making the end screen have a bit more stuff

--- Assets/script/allTrueChecker.cs
+++ Assets/script/allTrueChecker.cs
@@ -9,32 +9,11 @@ public class allTrueChecker : MonoBehaviour
 {
     public List<GameObject> objectcheck;
     public bool alltrue;
-    public bool fall;
     //public string currentSceneName = SceneManager.GetActiveScene().name;
     // Start is called before the first frame update
     void Start()
     {
     }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == "BuildObj")
-        {
-            fall = true;
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.tag == "BuildObj")
-        {
-            fall = false;
-        }
-    }
-
-
-
 
     // Update is called once per frame
     void Update()
@@ -48,11 +27,12 @@ public class allTrueChecker : MonoBehaviour
             {
                 alltrue = false;
             }
-
-            if(alltrue == true)
-            {
-                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-            }
         }
+
+        if (alltrue == true)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
     }
 }
